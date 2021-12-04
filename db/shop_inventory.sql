618da636281f2fe6ead03c322ddf0519d30420bd
Tamer <Tamer_amer@outlook.com>
2021-12-04 11:05:50 +0000

    fixed bug that made database not work

--- db/shop_inventory.sql
+++ db/shop_inventory.sql
@@ -1,6 +1,6 @@
-DROP TABLE product_categories;
-DROP TABLE manufacturers;
-DROP TABLE products;
+DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS product_categories;
+DROP TABLE IF EXISTS manufacturers;
 
 CREATE TABLE product_categories(
     id SERIAL PRIMARY KEY,
@@ -12,15 +12,15 @@ CREATE TABLE manufacturers(
     name VARCHAR(255),
     telephone_number INT,
     address VARCHAR(255)
-)
+);
 
-CREATE TABLE product(
+CREATE TABLE products(
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     description VARCHAR(255),
     purchase_price INT,
     selling_price INT,
     date_and_time VARCHAR(255),
-    manufacturer_id INT REFERENCES manufacturers(id) ON DELETE CASCADE,
-    product_category_id INT REFERENCES product_categories(id) ON DELETE CASCADE
+    manufacturer_id INT REFERENCES manufacturers(id),
+    product_category_id INT REFERENCES product_categories(id)
 );
